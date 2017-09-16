be80005a428c0d68ed03bcece254f8c370ca7292
h3rald <h3rald@h3rald.com>
2017-09-16 17:08:10 +0000

    Fixed command to detect CPU on linux.

--- agent/sensor_cpu.py
+++ agent/sensor_cpu.py
@@ -6,7 +6,7 @@ class CpuSensor:
     def __init__(self):
         platform = sys.platform
         if platform == 'linux':
-            self.command = 'top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F\'id,\' -v prefix="$prefix" \'{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%\n", prefix, 100 - v }\''
+            self.command = 'top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F\'id,\' -v prefix="$prefix" \'{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f", prefix, 100 - v }\''
         elif platform == 'darwin':
             # Assuming macOS
             self.command = "ps -A -o %cpu | awk '{s+=$1} END {print s}'"

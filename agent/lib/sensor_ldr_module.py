c54e44190fe5df67164a418d99c591deedceb15b
h3rald <h3rald@h3rald.com>
2018-01-05 09:40:36 +0100

    Added support for LDR module.

---
+++ agent/lib/sensor_ldr_module.py
@@ -0,0 +1,23 @@
+from utils import upython_sensor
+upython_sensor()
+from machine import Pin
+
+
+class LightSensor:
+
+    def __init__(self, id="", unit="LDR Module", pin=5, freq=5):
+        self.sensor_id = id
+        self.info = dict(
+            id = self.sensor_id,
+            type =  unit,
+            label='Light',
+            uom='',
+            freq = freq
+        )
+        self.ldr = Pin(pin)
+
+    def sample(self):
+        if self.ldr.value() == 0:
+            return 'on'
+        else:
+            return 'off'
